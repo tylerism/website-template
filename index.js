0601658724b52a5819d8fd1521bfaaef4de4c2db
Tyler Pugh <tylerpugh@tylers-air.lan>
2024-11-08 13:24:39 -0600

    Add cool face API

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 
@@ -8,4 +9,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
